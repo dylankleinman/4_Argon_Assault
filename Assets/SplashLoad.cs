c50f53df4147002115073df31b5d2840bff840bd
dylankleinman <dylankleinman@yahoo.com>
2019-07-25 22:58:43 -0700

    Inserted Ship, landscape, splash screen

---
+++ Assets/SplashLoad.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class SplashLoad : MonoBehaviour
+{
+    AudioSource audioSource;
+    [SerializeField] AudioClip themeMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("StopAudioSource", 5f);
+    }
+
+    private void StopAudioSource()
+    {
+        audioSource.Stop();
+    }
+
+}
