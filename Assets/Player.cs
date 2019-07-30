6ad3904f0d6d77b08a510376f87f7537c0078cb9
dylankleinman <dylankleinman@yahoo.com>
2019-07-29 18:56:09 -0700

    May Revert

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,10 +6,18 @@ using UnityStandardAssets.CrossPlatformInput; //allow cross platform input contr
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In meters/second")][SerializeField] float xSpeed = 4f;
-    [Tooltip("In meters/second")] [SerializeField] float ySpeed = 4f;
-    [SerializeField] float xClampRange = 10f;
-    [SerializeField] float yClampRange = 6f;
+    [Tooltip("In meters/second")][SerializeField] float xSpeed = 100f;
+    [Tooltip("In meters/second")] [SerializeField] float ySpeed = 100f;
+    [SerializeField] float xClampRange = 40f;
+    [SerializeField] float yClampRange = 35f;
+    [SerializeField] float positionPitchFactor = -0.4f;
+    [SerializeField] float controlPitchFactor = -9f;
+
+    [SerializeField] float positionYawFactor = -0.4f;
+
+    [SerializeField] float controlRollFactor = -15f;
+
+    float xThrow, yThrow;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,12 +34,20 @@ public class Player : MonoBehaviour
 
     private void ProcessRotation()
     {
-        transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
+        float pitchFromPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchFromControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchFromPosition + pitchFromControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = controlRollFactor * xThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     private void handleHorizontalInput()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float rawNewXPos = transform.localPosition.x + xOffset;
         float clampedXpos = Mathf.Clamp(rawNewXPos, -xClampRange, xClampRange);
@@ -39,7 +55,7 @@ public class Player : MonoBehaviour
     }
     private void handleVerticalInput()
     {
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * ySpeed * Time.deltaTime;
         float rawNewYPos = transform.localPosition.y + yOffset;
         float clampedYpos = Mathf.Clamp(rawNewYPos, -yClampRange, yClampRange);
